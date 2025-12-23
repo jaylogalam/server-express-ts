1d1859842a8cda79eed9ab50bae8e82609a67271
Jaylo <jaylogalam04@gmail.com>
2025-12-23 21:46:23 +0800

    auth: fixed router

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { corsMiddleware } from "./cors";
-import loginRouter from "./auth/routes/auth.router";
+import { authRouter } from "./auth/routes";
 import { connectDB } from "./db/mongodb";
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(corsMiddleware);
 
 app.use(express.json());
 
-app.use("/auth", loginRouter);
+app.use("/auth", authRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
