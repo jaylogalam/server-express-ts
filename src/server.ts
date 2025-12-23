c07aab37a503410f16a7d0cc4402de22fc6ccb2d
Jaylo <jaylogalam04@gmail.com>
2025-12-23 20:11:36 +0800

    cors: install cors and added to server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import express from "express";
+import { corsMiddleware } from "./cors";
+import loginRouter from "./auth/routes/auth.router";
 
 const app = express();
+
+app.use(corsMiddleware);
+
+app.use(express.json());
+
+app.use("/auth", loginRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
