1ee1812821b4c94762acf533bc8e79ccc93b7ea6
Jaylo <jaylogalam04@gmail.com>
2025-12-27 17:20:53 +0800

    check env when connecting. throw error if variables are missing

--- src/db/mongodb.ts
+++ src/db/mongodb.ts
@@ -14,11 +14,16 @@ if (!globalWithMongoose.mongoose) {
 const cached = globalWithMongoose.mongoose;
 
 export async function connectToDatabase(): Promise<Mongoose> {
+  if (!process.env.MONGODB_URI || !process.env.MONGODB_NAME)
+    throw new Error(
+      "Please provide a MongoDB URI and name in the environment variables"
+    );
+
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI!, {
-      dbName: process.env.MONGODB_NAME!,
+    cached.promise = mongoose.connect(process.env.MONGODB_URI, {
+      dbName: process.env.MONGODB_NAME,
     });
   }
 
