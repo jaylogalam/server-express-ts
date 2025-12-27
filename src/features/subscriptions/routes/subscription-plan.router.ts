978c2add15fbdfbb28a5e75e1341a963d476d2e1
Jaylo <jaylogalam04@gmail.com>
2025-12-27 17:20:22 +0800

    fix return data

--- src/features/subscriptions/routes/subscription-plan.router.ts
+++ src/features/subscriptions/routes/subscription-plan.router.ts
@@ -6,7 +6,7 @@ const router = Router();
 router.get("/", async (req: Request, res: Response) => {
   try {
     const plans = await plansServices.readPlans();
-    return res.json({ data: plans });
+    return res.json(plans);
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
@@ -15,7 +15,7 @@ router.get("/", async (req: Request, res: Response) => {
 router.post("/create", async (req: Request, res: Response) => {
   try {
     const plan = await plansServices.createPlan(req.body);
-    return res.json({ data: plan });
+    return res.json(plan);
   } catch (error: any) {
     return res.status(500).json({ error: error.message });
   }
